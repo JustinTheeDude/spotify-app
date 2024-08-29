28667bb86b1267ea6713e4159726b5714f33bf78
JustinTheeDude <justintheedude@gmail.com>
2024-08-29 03:46:56 -0700

    removed index test

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,9 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
-    globals: true,
     environment: 'jsdom',
-    setupFiles: "./setupTests.ts",
+    setupFiles: ["./setupTests.ts"],
   },
   resolve: {
     alias: [{ find: "@", replacement: resolve(__dirname, "./src") }]
